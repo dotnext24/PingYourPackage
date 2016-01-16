97b767084b19ec74d2e95538d90e92724763451f
dotnext24 <git>
2016-01-16 21:35:23 +0530

    config

---
+++ PingYourPackage/PingYourPackage.API/Config/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using PingYourPackage.API.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PingYourPackage.API.Config
+{
+   public class RouteConfig
+    {
+       public static void RegisterRoutes(HttpRouteCollection routes)
+       {
+           routes.MapHttpRoute(
+           "DefaultHttpRoute",
+           "api/{controller}/{key}",
+           defaults: new { key = RouteParameter.Optional },
+           constraints: new { key = new GuidRouteConstraint() });
+       }
+    }
+}
