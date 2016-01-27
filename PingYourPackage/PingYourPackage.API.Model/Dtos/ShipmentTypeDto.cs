6231401911d3267919b2a73abd7494c26ad129c1
dotnext24 <dotnext24@gmail.com>
2016-01-27 11:29:07 +0530

    dtos

---
+++ PingYourPackage/PingYourPackage.API.Model/Dtos/ShipmentTypeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WebApiDoodle.Net.Http.Client.Model;
+
+namespace PingYourPackage.API.Model.Dtos
+{
+    
+    public class ShipmentTypeDto : IDto {
+
+        public Guid Key { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
