c2c7f5f91e52bc24fdb5a735357d710340fa1002
dotnext24 <dotnext24@gmail.com>
2016-01-28 11:27:29 +0530

    requestcommand

--- PingYourPackage/PingYourPackage.API/Config/WebAPIConfig.cs
+++ PingYourPackage/PingYourPackage.API/Config/WebAPIConfig.cs
@@ -1,5 +1,6 @@
 ﻿using PingYourPackage.API.Formatting;
 using PingYourPackage.API.MessageHandlers;
+using PingYourPackage.API.Model.RequestCommands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,43 +11,49 @@ using System.Web.Http;
 using System.Web.Http.ModelBinding;
 using System.Web.Http.Validation;
 using System.Web.Http.Validation.Providers;
+using WebApiDoodle.Web.Filters;
 
 namespace PingYourPackage.API.Config
 {
-   public class WebAPIConfig
+    public class WebAPIConfig
     {
-       public static void Configure(HttpConfiguration config)
-       {
+        public static void Configure(HttpConfiguration config)
+        {
 
-           // Message Handlers
-           config.MessageHandlers.Add(new RequireHttpsMessageHandler());
-           config.MessageHandlers.Add(new PingYourPackageAuthHandler());
+            // Message Handlers
+            config.MessageHandlers.Add(new RequireHttpsMessageHandler());
+            config.MessageHandlers.Add(new PingYourPackageAuthHandler());
 
 
-           //Formatters
-           var jqueryFormatter = config.Formatters.FirstOrDefault(
-           x => x.GetType() ==
-           typeof(JQueryMvcFormUrlEncodedFormatter));
-           config.Formatters.Remove(
-           config.Formatters.FormUrlEncodedFormatter);
+            //Formatters
+            var jqueryFormatter = config.Formatters.FirstOrDefault(
+            x => x.GetType() ==
+            typeof(JQueryMvcFormUrlEncodedFormatter));
+            config.Formatters.Remove(
+            config.Formatters.FormUrlEncodedFormatter);
 
-           config.Formatters.Remove(jqueryFormatter);
+            config.Formatters.Remove(jqueryFormatter);
 
-           foreach (var formatter in config.Formatters)
-           {
-               formatter.RequiredMemberSelector =
-               new SuppressedRequiredMemberSelector();
-           }
+            foreach (var formatter in config.Formatters)
+            {
+                formatter.RequiredMemberSelector =
+                new SuppressedRequiredMemberSelector();
+            }
 
-           //Default Services
-           config.Services.Replace(
-           typeof(IContentNegotiator),
-           new DefaultContentNegotiator(
-           excludeMatchOnTypeOnly: true));
-           config.Services.RemoveAll(
-           typeof(ModelValidatorProvider),
-           validator => !(validator
-           is DataAnnotationsModelValidatorProvider));
-       }
+
+            // Filters
+            config.Filters.Add(
+            new InvalidModelStateFilterAttribute());
+
+            //Default Services
+            config.Services.Replace(typeof(IContentNegotiator), new DefaultContentNegotiator(
+            excludeMatchOnTypeOnly: true));
+            config.Services.RemoveAll(typeof(ModelValidatorProvider), validator => !(validator
+            is DataAnnotationsModelValidatorProvider));
+
+            // ParameterBindingRules
+            config.ParameterBindingRules.Insert(0,descriptor => typeof(IRequestCommand).IsAssignableFrom(descriptor.ParameterType)? new FromUriAttribute().GetBinding(descriptor) : null);
+
+        }
     }
 }
